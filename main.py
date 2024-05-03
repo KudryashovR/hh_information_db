2df338cada3a14abce0b713b98852ab2d4b90afb
KudryashovR <romkud55@gmail.com>
2024-05-04 02:30:12 +0300

    ADD: funcs for DB-work

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from pprint import pprint
-
-
-from src.utils import get_data_from_hh
+from src.utils import get_data_from_hh, insert_vacancies_into_db, insert_companies_into_db,clear_tables, create_tables
 
 
 def main():
@@ -9,7 +6,13 @@ def main():
         1942330, 49357, 78638, 2748, 1648566, 2180, 3529, 1942336, 196621, 4352
     ]
 
-    pprint(get_data_from_hh(emp_ids))
+    received_data = get_data_from_hh(emp_ids)
+    create_tables()
+    clear_tables()
+
+    for company_id, company_data in received_data.items():
+        insert_companies_into_db(company_id, company_data)
+        insert_vacancies_into_db(company_id, company_data.get("items"))
 
 
 if __name__ == "__main__":
